eca0c980c8bb5b2235fe5701f118a45accada1cc
Konstanty <kostu96@gmail.com>
2019-12-11 23:42:03 +0100

    Test

--- js/functions.js
+++ js/functions.js
@@ -26,18 +26,29 @@ class Question {
     }
 }
 
+var g_questions = [];
+
 function init() {
     var file = loadFile("resources/mii_questions.txt");
-    file = file.split('(');
+    file = file.split('\n');
     
     var questionNumber;
     var questionText;
     var questionAnswers = [];
     for (i = 0; i < file.length; ++i) {
-        questionNumber = parseInt(file[i].substr(0, file[i].indexOf(')') - 1));
-        alert(file[i]);
-        file[i] = file[i].substr(file[i].indexOf('\n') + 1);
-        alert(file[i]);
+        if (file[i][0] === '(') {
+
+            if (questionAnswers.length > 0)
+                g_questions.push(new Question(questionNumber, questionText, questionAnswers));
+
+            questionNumber = parseInt(file[i].substr(1, file[i].indexOf(')') - 1));
+            questionText = file[i].substr(file[i].indexOf(')') + 1);
+        }
+        else {
+            questionAnswers.push(file[i]);
+        }
     }
 
+    alert(g_questions[0].text);
+    alert(g_questions[0].answers);
 }
