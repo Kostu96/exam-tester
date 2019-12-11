64275cf59549d62e9713bf4316d11c7f3c920028
Konstanty <kostu96@gmail.com>
2019-12-11 23:57:23 +0100

    Test

--- js/functions.js
+++ js/functions.js
@@ -11,6 +11,21 @@ function loadFile(filePath) {
     return result;
 }
 
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+}
+
 class Answer {
     constructor(text, isCorrect) {
         this.text = text;
@@ -32,23 +47,24 @@ function init() {
     var file = loadFile("resources/mii_questions.txt");
     file = file.split('\n');
     
-    var questionNumber;
-    var questionText;
-    var questionAnswers = [];
+    var questionNumber, questionText, questionAnswers = [];
     for (i = 0; i < file.length; ++i) {
         if (file[i][0] === '(') {
 
-            if (questionAnswers.length > 0)
+            if (questionAnswers.length > 0) {
                 g_questions.push(new Question(questionNumber, questionText, questionAnswers));
+                questionAnswers = [];
+            }
 
             questionNumber = parseInt(file[i].substr(1, file[i].indexOf(')') - 1));
             questionText = file[i].substr(file[i].indexOf(')') + 1);
         }
-        else {
-            questionAnswers.push(file[i]);
-        }
+        else if (file[i][0] === 'X')
+            questionAnswers.push(new Answer(file[i].substr(2), true));
+        else
+            questionAnswers.push(new Answer(file[i], false));
     }
 
-    alert(g_questions[0].text);
-    alert(g_questions[0].answers);
+    g_questions = shuffle(g_questions);
+    document.getElementById("question").innerText(g_questions[0].text);
 }
