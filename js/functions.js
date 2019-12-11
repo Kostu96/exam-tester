f075eb2c77a9f448bcb7792270fb563913e9a975
Konstanty <kostu96@gmail.com>
2019-12-11 22:28:51 +0100

    Test opening files

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,21 @@
+function loadFile(filePath) {
+    var result = null;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", filePath, false);
+    xmlhttp.send();
+
+    if (xmlhttp.status == 200) {
+        result = xmlhttp.responseText;
+        alert(result);
+    }
+    else alert("error");
+
+    return result;
+}
 
 function init() {
+    var file = loadFile("resources/mii_questions.txt");
+    alert(file);
+
     var questionP = document.getElementById("question");
-    var file
 }
