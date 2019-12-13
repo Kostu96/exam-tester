0978cd4a2d156813bc00418e6617f06f9962c4a0
Konstanty <kostu96@gmail.com>
2019-12-13 21:43:13 +0100

    Add Stistics stub

---
+++ src/components/Statistics.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+class Statistics extends Component {
+    render() {
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Statistics;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import GlobalStyle from './GlobalStyle.js';
 import Header from './components/Header.js';
 import NavBar from './components/NavBar.js';
-import Footer from './components/Footer.js';
-import Question from './components/Question.js';
+import Statistics from './components/Statistics.js';
 import Settings from './components/Settings.js';
+import Question from './components/Question.js';
 import Ad from './components/Ad.js';
+import Footer from './components/Footer.js';
 
 class App extends Component {
     render() {
@@ -16,6 +17,7 @@ class App extends Component {
             <Header />
             <NavBar />
             <main>
+                <Statistics />
                 <Settings />
                 <Question />
             </main>
