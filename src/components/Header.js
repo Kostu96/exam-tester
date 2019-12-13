7ca6af55630aec26c1223285932b2973b3d44193
Konstanty <kostu96@gmail.com>
2019-12-13 16:53:24 +0100

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const header = styled.header`
+
+`;
+
+function Header() {
+    return(
+        <header>Exam Tester</header>
+    );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Header from './components/Header'
+
+function App() {
+    return (
+        <>
+            <Header />
+            <h2>Exam Tester v2.0 is comming...</h2>
+        </>
+    );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
