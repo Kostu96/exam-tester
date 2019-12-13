addae302e0c0a2806b044cf1a08fb79dfc469839
Konstanty <kostu96@gmail.com>
2019-12-13 22:41:43 +0100

    Read questions from file

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Header from './components/Header.js';
 import NavBar from './components/NavBar.js';
 import Statistics from './components/Statistics.js';
 import Settings from './components/Settings.js';
-import Question from './components/Question.js';
+import QuestionWrapper from './components/QuestionWrapper.js';
 import Ad from './components/Ad.js';
 import Footer from './components/Footer.js';
 
@@ -19,7 +19,38 @@ const Wrapper = styled.main`
     }
 `;
 
+class Answer {
+    constructor(text) {
+        this.text = text;
+        this.isCorrect = false;
+    }
+
+    makeCorrect() {
+        this.isCorrect = true;
+    }
+}
+
+class Question {
+    constructor(number, text, answers) {
+        this.number = number;
+        this.text = text;
+        this.answers = answers;
+        this.correntAttempts = 0;
+    }
+}
+
 class App extends Component {
+    state = {
+        questions: [],
+        questionCount: 0,
+        currentQuestionIndex: 0
+    }
+
+    constructor() {
+        super();
+        this.loadQuestionsData('/resources/mii_questions.txt');
+    }
+
     render() {
         return (
         <>
@@ -29,12 +60,47 @@ class App extends Component {
             <Wrapper>
                 <Statistics />
                 <Settings />
-                <Question />
+                <QuestionWrapper />
             </Wrapper>
             <Ad />
             <Footer />
         </>
         );
+    }
+
+    loadQuestionsData(filePath) {
+        var file = null;
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET", filePath, false);
+        xmlhttp.send();
+
+        if (xmlhttp.status === 200)
+            file = xmlhttp.responseText;
+        else alert("Error while loading questions database!");
+
+        file = file.split('\n');
+
+        var numberOfAnswers, questionText, questionAnswers = [];
+        this.state.questionCount = parseInt(file[0]);
+        file.shift();
+        for (var i = 0; i < this.state.questionCount; ++i) {
+            questionText = file[0].substr(file[0].indexOf(')') + 1);
+            file.shift();
+            numberOfAnswers = parseInt(file[0]);
+            file.shift();
+
+            for (var j = 0; j < numberOfAnswers; ++j) {
+                questionAnswers.push(new Answer(file[0]));
+                file.shift();
+            }
+
+            var num = parseInt(file[0]);
+            questionAnswers[num - 1].makeCorrect();
+            file.shift();
+
+            this.state.questions.push(new Question(i + 1, questionText, questionAnswers));
+            questionAnswers = [];
+        }
     }
 }
 
