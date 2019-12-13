d4b29e2c07f98991a31731289640eccba575c898
Konstanty <kostu96@gmail.com>
2019-12-13 17:46:09 +0100

    Change header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,16 @@ import styled from 'styled-components';
 const PageHeader = styled.header`
     text-align: center;
     padding: 20px;
+    margin-bottom: 10px;
     background-color: #222628;
-    font-size: 32px;
+    font-size: 35px;
+    font-weight: 700;
+    letter-spacing: 3px;
 `;
 
 function Header() {
     return(
-        <PageHeader>Exam Tester</PageHeader>
+        <PageHeader>EXAM TESTER</PageHeader>
     );
 }
 
