4f81e5576f9e8e5beb03187a4864339e876e3600
Konstanty <kostu96@gmail.com>
2019-12-13 17:13:52 +0100

    Fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const page_header = styled.header`
+const PageHeader = styled.header`
     text-align: center;
-    padding: 30px;
-    background-color: #252525;
-    font-size: 36px;
+    padding: 20px;
+    background-color: #222628;
+    font-size: 32px;
 `;
 
 function Header() {
     return(
-        <page_header>Exam Tester</page_header>
+        <PageHeader>Exam Tester</PageHeader>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import './GlobalStyle.js'
-import Header from './components/Header.js'
+import GlobalStyle from './GlobalStyle.js';
+import Header from './components/Header.js';
 
 function App() {
     return (
         <>
+            <GlobalStyle />
             <Header />
             <h2>Exam Tester v2.0 is comming...</h2>
         </>
